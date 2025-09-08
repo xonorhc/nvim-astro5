c1121c2d37aba229af55eccac2897926f1635c47
xonorhc <xonorhc@proton.me>
2025-09-08 11:08:26 -0300

    chage yarn to npm

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -24,7 +24,7 @@ return {
   init = function()
     local plugin = require("lazy.core.config").spec.plugins["markdown-preview.nvim"]
     vim.g.mkdp_filetypes = require("lazy.core.plugin").values(plugin, "ft", true)
-    vim.g.mkdp_markdown_css = "/home/xonorhc/Dropbox/tech/css/markdown-air.css"
+    -- vim.g.mkdp_markdown_css = "/home/xonorhc/Dropbox/tech/css/markdown-air.css"
     vim.g.mkdp_preview_options = { disable_filename = true, }
   end,
   dependencies = {
