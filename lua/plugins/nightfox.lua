9b6694be363e3269c759e73a43e38a3f65ffc3df
xonorhc <xonorhc@proton.me>
2025-09-12 12:32:17 -0300

    add new colorscheme
    nightfox pre-configured with oxocarbon aka carbonfox

---
+++ lua/plugins/nightfox.lua
@@ -0,0 +1,38 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+---@type LazySpec
+return {
+  "EdenEast/nightfox.nvim",
+  lazy = true,
+  opts = {
+    options = {
+      transparent = true,
+      module_default = false,
+      modules = {
+        aerial = true,
+        cmp = true,
+        ["dap-ui"] = true,
+        dashboard = true,
+        diagnostic = true,
+        gitsigns = true,
+        native_lsp = true,
+        neotree = true,
+        notify = true,
+        symbol_outline = true,
+        telescope = true,
+        treesitter = true,
+        whichkey = true,
+      },
+    },
+    groups = { all = { NormalFloat = { link = "Normal" } } },
+  },
+
+  -- change colorscheme
+  {
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      colorscheme = "carbonfox",
+    },
+  },
+}
