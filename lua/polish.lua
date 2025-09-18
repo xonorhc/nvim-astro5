d17c37b8ee5dcb6c456c56bd11ecb6e5408c074c
xonorhc <xonorhc@proton.me>
2025-09-18 17:02:19 -0300

    refact
    remove auto close window

--- lua/polish.lua
+++ lua/polish.lua
@@ -6,39 +6,27 @@
 
 local map = vim.keymap.set
 
--- Python built-in terminal: {{{
--- map("n", "<leader>r", "<Cmd>w<CR> <Cmd>below split | term python3 %<CR> i", { noremap = true, silent = true })
--- }}}
+map("n", "<A-r>", "<Cmd>w<CR> <Cmd>below split | term python3 %<CR> i", { noremap = true, silent = true })
 
 -- Python built-in terminal: {{{
 -- map("n", "<F5>", function()
 --   vim.cmd "w"
---   vim.cmd "below split | term python %"
+--   vim.cmd "below vsplit | term python %"
 --   vim.cmd "startinsert"
 -- end, { desc = "Python built-in terminal" })
--- }}}
-
--- Run .py file via terminal: {{{
--- local function run_curr_python_file()
---   local file_name = vim.api.nvim_buf_get_name(0)
---   local py_cmd = vim.api.nvim_replace_termcodes('python "' .. file_name .. '"<cr>', true, false, true)
---   vim.cmd ":ToggleTerm direction=float"
---   vim.api.nvim_feedkeys(py_cmd, "t", false)
--- end
--- map({ "n" }, "<A-r>", "", {
---   desc = "Run .py file via terminal",
---   callback = run_curr_python_file,
--- })
 -- }}}
 
 -- ToggleTerm python: {{{
 -- local astro = require "astrocore"
 -- local python = vim.fn.executable "python" == 1 and "python" or vim.fn.executable "python3" == 1 and "python3"
--- if python then map("n", "<Leader>r", function() astro.toggle_term_cmd(python) end, { desc = "ToggleTerm python" }) end
+-- if python then map("n", "<Leader>tp", function() astro.toggle_term_cmd(python) end, { desc = "ToggleTerm python" }) end
 -- }}}
 
+-- ToggleTerm python: {{{
 local Terminal = require("toggleterm.terminal").Terminal
 local file_name = vim.api.nvim_buf_get_name(0)
-local python = Terminal:new { cmd = 'python "' .. file_name .. '"', hidden = true, direction = "float" }
+local python =
+  Terminal:new { cmd = 'python "' .. file_name .. '"', hidden = false, direction = "float", close_on_exit = false }
 function _python_toggle() python:toggle() end
 vim.api.nvim_set_keymap("n", "<leader>r", "<cmd>lua _python_toggle() <CR> ", { noremap = true, silent = true })
+-- }}}
