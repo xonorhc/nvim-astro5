ddd23363c44099472e06bf7c9f17102d2c411a09
xonorhc <xonorhc@proton.me>
2025-09-25 23:01:32 -0300

    add plugin jupynium
    Plugin to edit nvim and "preview" jupiter notebook using selenium

---
+++ lua/plugins/jupynium.lua
@@ -0,0 +1,12 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  {
+    "kiyoon/jupynium.nvim",
+    build = "pip3 install --user .",
+    -- build = "uv pip install . --python=$HOME/.virtualenvs/jupynium/bin/python",
+    -- build = "conda run --no-capture-output -n jupynium pip install .",
+  },
+  "rcarriga/nvim-notify", -- optional
+  "stevearc/dressing.nvim", -- optional, UI for :JupyniumKernelSelect
+}
