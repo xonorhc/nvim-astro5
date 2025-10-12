3a9cd74726de0bca3681745718b5457fc90ce55f
xonorhc <xonorhc@proton.me>
2025-10-12 00:01:56 -0300

    update
    Adjust colorscheme and polish

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -7,7 +7,7 @@ return {
     name = "rose-pine",
     lazy = true,
     opts = {
-      variant = "auto", -- auto, main, moon, or dawn
+      variant = "moon", -- auto, main, moon, or dawn
       dark_variant = "main", -- main, moon, or dawn
       dim_inactive_windows = false,
       extend_background_behind_borders = true,
--- lua/polish.lua
+++ lua/polish.lua
@@ -6,27 +6,29 @@
 
 local map = vim.keymap.set
 
-map("n", "<A-r>", "<Cmd>w<CR> <Cmd>below split | term python3 %<CR> i", { noremap = true, silent = true })
+map("n", "<A-p>", "<Cmd>w<CR> <Cmd>below split | term python3 %<CR> i", { noremap = true, silent = true })
 
--- Python built-in terminal: {{{
+map("n", "<A-j>", "<Cmd>w<CR> <Cmd>below split | term node %<CR> i", { noremap = true, silent = true })
+
+-- -- Python built-in terminal: {{{
 -- map("n", "<F5>", function()
 --   vim.cmd "w"
 --   vim.cmd "below vsplit | term python %"
 --   vim.cmd "startinsert"
 -- end, { desc = "Python built-in terminal" })
--- }}}
+-- -- }}}
 
--- ToggleTerm python: {{{
+-- -- ToggleTerm python: {{{
 -- local astro = require "astrocore"
 -- local python = vim.fn.executable "python" == 1 and "python" or vim.fn.executable "python3" == 1 and "python3"
 -- if python then map("n", "<Leader>tp", function() astro.toggle_term_cmd(python) end, { desc = "ToggleTerm python" }) end
--- }}}
+-- -- }}}
 
--- ToggleTerm python: {{{
-local Terminal = require("toggleterm.terminal").Terminal
-local file_name = vim.api.nvim_buf_get_name(0)
-local python =
-  Terminal:new { cmd = 'python "' .. file_name .. '"', hidden = false, direction = "float", close_on_exit = false }
-function _python_toggle() python:toggle() end
-vim.api.nvim_set_keymap("n", "<leader>r", "<cmd>lua _python_toggle() <CR> ", { noremap = true, silent = true })
--- }}}
+-- -- ToggleTerm python: {{{
+-- local Terminal = require("toggleterm.terminal").Terminal
+-- local file_name = vim.api.nvim_buf_get_name(0)
+-- local python =
+--   Terminal:new { cmd = 'python "' .. file_name .. '"', hidden = false, direction = "float", close_on_exit = false }
+-- function _python_toggle() python:toggle() end
+-- vim.api.nvim_set_keymap("n", "<leader>r", "<cmd>lua _python_toggle() <CR> ", { noremap = true, silent = true })
+-- -- }}}
