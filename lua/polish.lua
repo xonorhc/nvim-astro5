d19d3332bed3910d64378f65b8efddc3e3fcf216
xonorhc <xonorhc@proton.me>
2025-10-12 00:02:15 -0300

    update
    Adjust polish

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,4 +1,4 @@
--- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
