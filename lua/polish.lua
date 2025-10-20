3886b6e0ccc88332fbd94f51411018a3a62f2fd2
xonorhc <xonorhc@proton.me>
2025-10-20 15:49:15 -0300

    update
    add run for nodejs

--- lua/polish.lua
+++ lua/polish.lua
@@ -7,15 +7,17 @@ if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 local map = vim.keymap.set
 
 map("n", "<A-n>", "<Cmd>w<CR> <Cmd>below split | term node %<CR> i", { noremap = true, silent = true })
+map("n", "<A-p>", "<Cmd>w<CR> <Cmd>below split | term python3 %<CR> i", { noremap = true, silent = true })
 
-map("n", "<F5>", function()
-  vim.cmd "w"
-  vim.cmd "below vsplit | term python %"
-  vim.cmd "startinsert"
-end, { desc = "Python built-in terminal" })
+-- map("n", "<F5>", function()
+--   vim.cmd "w"
+--   vim.cmd "below vsplit | term python %"
+--   vim.cmd "startinsert"
+-- end, { desc = "Python built-in terminal" })
 
 local terminal = require("toggleterm.terminal").Terminal
 local file_name = vim.api.nvim_buf_get_name(0)
+
 local python = terminal:new {
   cmd = 'python "' .. file_name .. '"',
   hidden = false,
@@ -24,4 +26,14 @@ local python = terminal:new {
   start_in_insert = true,
 }
 function _python_toggle() python:toggle() end
-vim.api.nvim_set_keymap("n", "<A-p>", "<cmd>lua _python_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>rp", "<cmd>lua _python_toggle()<CR>", { noremap = true, silent = true })
+
+local node = terminal:new {
+  cmd = 'node "' .. file_name .. '"',
+  hidden = false,
+  direction = "float",
+  close_on_exit = false,
+  start_in_insert = true,
+}
+function _node_toggle() node:toggle() end
+vim.api.nvim_set_keymap("n", "<Leader>rn", "<cmd>lua _node_toggle()<CR>", { noremap = true, silent = true })
