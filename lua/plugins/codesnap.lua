4756f778449c4bc6f1e11f262aab1622bdde664e
xonorhc <xonorhc@proton.me>
2025-10-31 14:32:19 -0300

    feat: Add plugins
    Add plugins:
    - CodeSnap for pretty code image
    - Full-dadbod to database connection and run queries
    - Live-server to preview html files
    - Markdown-preview to view md files into browser
    - Yazi to management files

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,28 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+local prefix = "<Leader>c"
+return {
+  "mistricky/codesnap.nvim",
+  build = "make",
+  cmd = { "CodeSnap", "CodeSnapSave", "CodeSnapHighlight", "CodeSnapSaveHighlight" },
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          v = {
+            [prefix] = { desc = " CodeSnap" },
+            [prefix .. "s"] = { ":'<,'>CodeSnap<CR>", desc = "CodeSnap (clipboard)" },
+            [prefix .. "S"] = { ":'<,'>CodeSnapSave<CR>", desc = "CodeSnap (save)" },
+            [prefix .. "h"] = { ":'<,'>CodeSnapHighlight<CR>", desc = "CodeSnap with highlight (clipboard)" },
+            [prefix .. "H"] = { ":'<,'>CodeSnapSaveHighlight<CR>", desc = "CodeSnap with highlight (save)" },
+          },
+        },
+      },
+    },
+  },
+  opts = {
+    mac_window_bar = false,
+    watermark = "",
+  },
+}
---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,48 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+---@type LazySpec
+return {
+  "iamcco/markdown-preview.nvim",
+  build = function(plugin)
+    local package_manager = vim.fn.executable "npm" and "npm" or vim.fn.executable "npx" and "npx -y yarn" or false
+
+    --- HACK: Use `yarn` or `npx` when possible, otherwise throw an error
+    ---@see https://github.com/iamcco/markdown-preview.nvim/issues/690
+    ---@see https://github.com/iamcco/markdown-preview.nvim/issues/695
+    if not package_manager then error "Missing `yarn` or `npx` in the PATH" end
+
+    local cmd = string.format(
+      "!cd %s && cd app && COREPACK_ENABLE_AUTO_PIN=0 %s install --frozen-lockfile",
+      plugin.dir,
+      package_manager
+    )
+
+    vim.cmd(cmd)
+  end,
+  ft = { "markdown", "markdown.mdx" },
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  init = function()
+    local plugin = require("lazy.core.config").spec.plugins["markdown-preview.nvim"]
+    vim.g.mkdp_filetypes = require("lazy.core.plugin").values(plugin, "ft", true)
+    vim.g.mkdp_browser = "firefox"
+    vim.g.mkdp_theme = "dark"
+    vim.g.mkdp_markdown_css = "~/Documents/css/markdown-air.css"
+    vim.g.mkdp_preview_options = { disable_filename = true }
+  end,
+  dependencies = {
+    { "AstroNvim/astroui", opts = { icons = { Markdown = "" } } },
+    {
+      "AstroNvim/astrocore",
+      optional = true,
+      opts = function(_, opts)
+        local maps = opts.mappings
+        local prefix = "<Leader>M"
+
+        maps.n[prefix] = { desc = require("astroui").get_icon("Markdown", 1, true) .. "Markdown" }
+        maps.n[prefix .. "p"] = { "<cmd>MarkdownPreview<cr>", desc = "Preview" }
+        maps.n[prefix .. "s"] = { "<cmd>MarkdownPreviewStop<cr>", desc = "Stop preview" }
+        maps.n[prefix .. "t"] = { "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle preview" }
+      end,
+    },
+  },
+}
