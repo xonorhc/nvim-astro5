c90c4a574864a5f24330496a5c527159a4f10c9a
anachronox <hgolin@tutanota.com>
2025-12-26 17:16:40 -0300

    feat: Add colorscheme Decay
    Add new colorscheme: Decay

---
+++ lua/plugins/decay.lua
@@ -0,0 +1,11 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+---@type LazySpec
+return {
+  "decaycs/decay.nvim",
+  name = "decay",
+  lazy = true,
+  config = function()
+    -- SNIP
+  end,
+}
