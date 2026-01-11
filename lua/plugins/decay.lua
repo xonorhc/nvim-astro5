42625f538532ca1988a79182fad6704780bc902d
xonorhc <xonorhc@proton.me>
2026-01-11 13:06:00 -0300

    fix: colorscheme decay
    update transparency to colorscheme decay

--- lua/plugins/decay.lua
+++ lua/plugins/decay.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 ---@type LazySpec
 return {
@@ -6,6 +6,33 @@ return {
   name = "decay",
   lazy = true,
   config = function()
-    -- SNIP
+    local decay = require "decay"
+
+    local opt = vim.opt
+
+    opt.background = "dark"
+
+    decay.setup {
+      style = "default",
+
+      -- enables italics in code keywords & comments.
+      italics = {
+        code = true,
+        comments = true,
+      },
+
+      -- enables contrast when using nvim tree.
+      nvim_tree = {
+        contrast = true,
+      },
+
+      cmp = {
+        block_kind = false,
+      },
+
+      palette_overrides = {
+        background = "none",
+      },
+    }
   end,
 }
