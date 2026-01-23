bb74fab5565af68a442fec8cb891bc72be08c5b1
chronox <xonorhc@proton.me>
2026-01-22 23:55:29 -0300

    Update rose-pine

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -4,54 +4,19 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "rose-pine/neovim",
   name = "rose-pine",
-  lazy = true,
-  opts = {
-    variant = "moon",
-    dark_variant = "main",
-    dim_inactive_windows = false,
-    extend_background_behind_borders = true,
+  config = function()
+    require("rose-pine").setup {
+      extend_background_behind_borders = false,
 
-    enable = {
-      terminal = true,
-      legacy_highlights = true,
-      migrations = true,
-    },
+      styles = {
+        bold = false,
+        italic = false,
+        transparency = true,
+      },
 
-    styles = {
-      bold = true,
-      italic = true,
-      transparency = true,
-    },
-
-    groups = {
-      border = "muted",
-      link = "iris",
-      panel = "surface",
-
-      error = "love",
-      hint = "iris",
-      info = "foam",
-      note = "pine",
-      todo = "rose",
-      warn = "gold",
-
-      git_add = "foam",
-      git_change = "rose",
-      git_delete = "love",
-      git_dirty = "rose",
-      git_ignore = "muted",
-      git_merge = "iris",
-      git_rename = "pine",
-      git_stage = "iris",
-      git_text = "rose",
-      git_untracked = "subtle",
-
-      h1 = "iris",
-      h2 = "foam",
-      h3 = "rose",
-      h4 = "gold",
-      h5 = "pine",
-      h6 = "foam",
-    },
-  },
+      highlight_groups = {
+        Comment = { italic = true },
+      },
+    }
+  end,
 }
