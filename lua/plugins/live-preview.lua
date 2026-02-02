b5e83bcfa263acdd01850f7ec46753a3f968290b
xonorhc <xonorhc@proton.me>
2026-02-02 14:07:55 -0300

    feat(plugins): live preview from html and markdown files

---
+++ lua/plugins/live-preview.lua
@@ -0,0 +1,12 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  "brianhuster/live-preview.nvim",
+  dependencies = {
+    -- You can choose one of the following pickers
+    -- "nvim-telescope/telescope.nvim",
+    -- "ibhagwan/fzf-lua",
+    -- "echasnovski/mini.pick",
+    "folke/snacks.nvim",
+  },
+}
