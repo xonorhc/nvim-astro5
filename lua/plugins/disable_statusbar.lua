3d34412e5ccb29c4eebaad99c4268362a3efac4b
xonorhc <xonorhc@proton.me>
2026-02-02 14:00:11 -0300

    feat(astrocore): disable statusbar

---
+++ lua/plugins/disable_statusbar.lua
@@ -0,0 +1,15 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@param opts AstroCoreOpts
+    opts = {
+      options = {
+        opt = {
+          laststatus = 0,
+        },
+      },
+    },
+  },
+}
