804839fb86620ee8c0f8f1f3de2e170f6b131d29
xonorhc <xonorhc@proton.me>
2026-02-02 14:06:08 -0300

    feat(plugins): add lazydocker for container management

---
+++ lua/plugins/lazydocker.lua
@@ -0,0 +1,26 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+---@type LazySpec
+return {
+  {
+    "mgierada/lazydocker.nvim",
+    lazy = true,
+    dependencies = {
+      "akinsho/toggleterm.nvim",
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+
+          -- INFO: Default prefix of toggleterm-related actions
+          local prefix = "<Leader>t"
+
+          maps.n[prefix .. "d"] = {
+            function() require("lazydocker").open() end,
+            desc = "ToggleTerm LazyDocker",
+          }
+        end,
+      },
+    },
+  },
+}
